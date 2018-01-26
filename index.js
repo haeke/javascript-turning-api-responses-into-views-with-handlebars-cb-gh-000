4f05b31199659d1cabf582f6f4b48c47b2cf29da
edwin rivera <waehake@gmail.com>
2018-01-26 15:41:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function showRepositories(event, data) {
   //map over the repolist object
   //add each repo name as a list item
   //use join so there are no commas
-  const repoList = `<ul>${repos.map(res => '<li><a href="' + res.html_url + '">' + + res.name + '</li>').join('')} </ul>`;
+  const repoList = `<ul>${repos.map(res => '<li><a href="' + res.html_url + '">' + res.name + '</li>').join('')} </ul>`;
   //show the data in the div with id named repositories
   document.getElementById("repositories").innerHTML = repoList;
 }
